a6e90be918648d294d4a0a3271b72ddc5daf356b
Dawood Hashmi <dhashmi36@gmail.com>
2024-08-08 23:48:54 -0400

    client changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,31 @@ $(document).ready(function(){
        $(".header-right nav").slideToggle();
    });
 
+//    slick slider
+    $(".slick-slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        speed: 300,
+        infinite: true,
+        autoplaySpeed: 5000,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
 });
